e231a017836bf239c50aa3b56b4251a0e315f228
Saimonitsa <95011357+Saimonitsa@users.noreply.github.com>
2021-12-27 12:15:08 +0500

    5

--- лаба ооп6/Form1.cs
+++ лаба ооп6/Form1.cs
@@ -19,204 +19,357 @@ namespace лаба_ооп6
             (pictureBox1 as Control).KeyPress += new KeyPressEventHandler(PressEventHandler);
         }
 
-        public abstract class Shape : ObjObserved
+        public void PressEventHandler(object sender, KeyPressEventArgs e)
         {
-            protected Rectangle rect; //область объекта для его отрисовки и выделения
-            protected int x, y, width, height; //x, y для позиции объектов и групп, w и h для учитывания границ отрисовки
-            public bool sticky = false;
-            abstract public void Resize();
-            abstract public void SetXY(int _x, int _y);
-            abstract public void OffsetXY(int _x, int _y);
-            abstract public void SetColor(Color c);
-            abstract public void Grow(int gr);
-            abstract public void DrawObj(System.Drawing.Graphics e);
-            abstract public void DrawRectangle(System.Drawing.Graphics e, Pen pen);
-            abstract public bool Find(int _x, int _y);
-            abstract public Rectangle GetRectangle();  //получить границы фигуры для контроля выхода за пределы
+            if (MyStorage.size() != 0)
+            {
+                if (e.KeyChar == 119) MyStorage.get().OffsetXY(0, -1);
+                if (e.KeyChar == 115) MyStorage.get().OffsetXY(0, 1);
+                if (e.KeyChar == 97) MyStorage.get().OffsetXY(-1, 0);
+                if (e.KeyChar == 100) MyStorage.get().OffsetXY(1, 0);
+                if (e.KeyChar == 98) MyStorage.get().Grow(1);
+                if (e.KeyChar == 118) MyStorage.get().Grow(-1);
+                if (e.KeyChar == 110)
+                {
+                    while (MyStorage.size() != 0) MyStorage.del();
+                    pictureBox1.Invalidate();
+                }
+                if (e.KeyChar == 112)
+                {
+
+                    if (colorDialog1.ShowDialog() == DialogResult.OK) MyStorage.get().SetColor(colorDialog1.Color);
+                }
+
+                Random rnd = new Random();
+                int obj = rnd.Next(1, 3);
+                if (e.KeyChar == 49)
+                {
+                    int rad = rnd.Next(10, 100);
+                    MyStorage.add(new CCircle(rnd.Next(4, pictureBox1.Width - 50), rnd.Next(4, pictureBox1.Height - 50), rad, Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)), pictureBox1.Width - 50, pictureBox1.Height - 50));
+                }
+                if (e.KeyChar == 51)
+                {
+                    int rad = rnd.Next(10, 100);
+                    MyStorage.add(new Square(rnd.Next(4, pictureBox1.Width - 50), rnd.Next(4, pictureBox1.Height - 50), rad, Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)), pictureBox1.Width - 50, pictureBox1.Height - 50));
+                }
+                if (e.KeyChar == 50)
+                {
+                    int rad = rnd.Next(10, 100);
+                    MyStorage.add(new Triangle(rnd.Next(4, pictureBox1.Width - 50), rnd.Next(4, pictureBox1.Height - 50), rad, Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)), pictureBox1.Width - 50, pictureBox1.Height - 50));
+                }
+
+            }
+            pictureBox1.Invalidate();
         }
-            public void PressEventHandler(object sender, KeyPressEventArgs e)
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            bool isFinded = false;
+            if (MyStorage.size() != 0)
             {
-                if (MyStorage.size() != 0)
+                MyStorage.toFirst();
+                for (int i = 0; i < MyStorage.size(); i++, MyStorage.next())
                 {
-                    if (e.KeyChar == 119) MyStorage.get().OffsetXY(0, -1);
-                    if (e.KeyChar == 115) MyStorage.get().OffsetXY(0, 1);
-                    if (e.KeyChar == 97) MyStorage.get().OffsetXY(-1, 0);
-                    if (e.KeyChar == 100) MyStorage.get().OffsetXY(1, 0);
-                    if (e.KeyChar == 98) MyStorage.get().Grow(1);
-                    if (e.KeyChar == 118) MyStorage.get().Grow(-1);
-                    if (e.KeyChar == 110)
+                    if (MyStorage.getIterator().Find(e.X, e.Y) == true && e.Button == MouseButtons.Left)
                     {
-                        while (MyStorage.size() != 0) MyStorage.del();
-                        pictureBox1.Invalidate();
+                        isFinded = true;
+                        MyStorage.setCurPTR();
+                        break;
                     }
-                    if (e.KeyChar == 112)
+                    if (MyStorage.getIterator().Find(e.X, e.Y) == true && e.Button == MouseButtons.Right)
                     {
-
-                        if (colorDialog1.ShowDialog() == DialogResult.OK) myStorage.get().SetColor(colorDialog1.Color);
+                        isFinded = true;
+                        MyStorage.check();
+                        break;
                     }
-
+                }
+            }
+            if (isFinded == false)
+            {
+                if (radioButton1.Checked == true)
+                {
                     Random rnd = new Random();
-                    int obj = rnd.Next(1, 3);
-                    if (e.KeyChar == 49)
-                    {
-                        int rad = rnd.Next(10, 100);
-                        MyStorage.add(new Circle(rnd.Next(4, pictureBox1.Width - 50), rnd.Next(4, pictureBox1.Height - 50), rad, Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)), pictureBox1.Width - 50, pictureBox1.Height - 50));
-                    }
-                    if (e.KeyChar == 51)
-                    {
-                        int rad = rnd.Next(10, 100);
-                        MyStorage.add(new Polygon(rnd.Next(4, pictureBox1.Width - 50), rnd.Next(4, pictureBox1.Height - 50), rad, rnd.Next(3, 9), Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)), pictureBox1.Width - 50, pictureBox1.Height - 50));
-                    }
-                    if (e.KeyChar == 50)
-                    {
-                        int rad = rnd.Next(10, 100);
-                        MyStorage.add(new Star(rnd.Next(4, pictureBox1.Width - 50), rnd.Next(4, pictureBox1.Height - 50), rad, rnd.Next(5, 9), Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)), pictureBox1.Width - 50, pictureBox1.Height - 50));
-                    }
-
+                    int rad = rnd.Next(10, 100);
+                    MyStorage.add(new CCircle(e.X, e.Y, rad, Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)), pictureBox1.Width, pictureBox1.Height));
                 }
-                pictureBox1.Invalidate();
+                else
+                if (radioButton2.Checked == true)
+                {
+                    Random rnd = new Random();
+                    int rad = rnd.Next(10, 100);
+                    MyStorage.add(new Square(e.X, e.Y, rad, Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)), pictureBox1.Width, pictureBox1.Height));
+                }
+                else
+                if (radioButton3.Checked == true)
+                {
+                    Random rnd = new Random();
+                    int rad = rnd.Next(10, 100);
+                    MyStorage.add(new Triangle(e.X, e.Y, rad, Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)), pictureBox1.Width, pictureBox1.Height));
+                }
+
             }
+            pictureBox1.Invalidate();
+        }
 
-
-            private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (MyStorage.size() != 0)
             {
-                bool isFinded = false;
-                if (MyStorage.size() != 0)
+                MyStorage.toFirst();
+                for (int i = 0; i < MyStorage.size(); i++, MyStorage.next())
                 {
-                    MyStorage.toFirst();
-                    for (int i = 0; i < MyStorage.size(); i++, MyStorage.next())
-                    {
-                        if (MyStorage.getIterator().Find(e.X, e.Y) == true && e.Button == MouseButtons.Left)
-                        {
-                            isFinded = true;
-                            MyStorage.setCurPTR();
-                            break;
-                        }
-                        if (MyStorage.getIterator().Find(e.X, e.Y) == true && e.Button == MouseButtons.Right)
-                        {
-                            isFinded = true;
-                            MyStorage.check();
-                            break;
-                        }
-                    }
+                    MyStorage.getIterator().DrawObj(e.Graphics);
+                    if (MyStorage.isChecked() == true) MyStorage.getIterator().DrawRectangle(e.Graphics, new Pen(Color.Gray, 2));
                 }
-                if (isFinded == false)
-                {
-                    if (radioButton3.Checked == true)
-                    {
-                        Random rnd = new Random();
-                        int rad = rnd.Next(10, 100);
-                        MyStorage.add(new Circle(e.X, e.Y, rad, Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)), pictureBox1.Width, pictureBox1.Height));
-                    }
-                    else
-                    if (radioButton2.Checked == true)
-                    {
-                        Random rnd = new Random();
-                        int rad = rnd.Next(10, 100);
-                        MyStorage.add(new Polygon(e.X, e.Y, rad, rnd.Next(3, 9), Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256)), pictureBox1.Width, pictureBox1.Height));
-                        ((Polygon)MyStorage.get()).Rotate(rnd.Next(0, 180));
-                    }
-                }
-                pictureBox1.Invalidate();
+                MyStorage.get().DrawRectangle(e.Graphics, new Pen(Color.Red, 1));
             }
-
-            private void pictureBox1_Paint(object sender, PaintEventArgs e)
-            {
-                if (MyStorage.size() != 0)
-                {
-                    MyStorage.toFirst();
-                    for (int i = 0; i < MyStorage.size(); i++, MyStorage.next())
-                    {
-                        MyStorage.getIterator().DrawObj(e.Graphics);
-                        if (MyStorage.isChecked() == true) MyStorage.getIterator().DrawRectangle(e.Graphics, new Pen(Color.Gray, 2));
-                    }
-                    MyStorage.get().DrawRectangle(e.Graphics, new Pen(Color.Red, 1));
-                }
-            }
+        }
 
 
-            private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
-            {
-                pictureBox1.Focus();
-            }
-
-
-
-            public class CCircle : Shape
-            {
-                protected Color color;
-                protected int R;
-                public CCircle()
-                {
-                    X = 0;
-                    Y = 0;
-                    R = 0;
-                }
-                public CCircle(int x, int y, int r, Color c, int width, int height)
-                {
-
-                    this.X = x;
-                    this.Y = y;
-                    width = Width;
-                    height = Height;
-                    color = c;
-                    if (r > x) r = x;
-                    if (x + r > width) r = width - x;
-                    if (r > y) r = y;
-                    if (y + r > height) r = height - y;
-                    R = r;
-                    rect = new Rectangle(x - R, y - R, 2 * R, 2 * R);
-
-                }
-                public override void SetXY(int x, int y)
-                {
-                    throw new NotImplementedException();
-                }
-                public override void Resize()
-                {
-                    rect = new Rectangle(x - R, y - R, 2 * R, 2 * R);
-                }
-
-                public override void Grow(int inc)
-                {
-                    if (R + inc < x && x + R + inc < width && R + inc < y && y + radius + inc < height && radius + inc > 0) radius += inc;
-                    Resize();
-                }
-
-                public override void SetColor(Color c)
-                {
-                    color = c;
-                }
-
-                public override void DrawObj(Graphics e)
-                {
-                    e.FillEllipse(new SolidBrush(color), rect);
-                }
-
-                public override void DrawRectangle(Graphics e, Pen pen)
-                {
-                    e.DrawRectangle(pen, rect);
-                }
-
-                public override void OffsetXY(int _x, int _y)
-                {
-                    if (x + _x > R && x + _x + R < width) x += _x;
-                    if (y + _y > R && y + _y + R < height) y += _y;
-                    Resize();
-                }
-                public override Rectangle GetRectangle()
-                {
-                    return rect;
-                }
-
-                public override bool Find(int _x, int _y)
-                {
-                    if (Math.Pow(x - _x, 2) + Math.Pow(y - _y, 2) <= R * radius) return true; else return false;
-                }
-            }
-        
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Focus();
+        }
     }
+
 };
+public abstract class Shape : ObjObserved
+{
+    protected Rectangle rect; //область объекта для его отрисовки и выделения
+    protected int X, Y, width, height; //x, y для позиции объектов и групп, w и h для учитывания границ отрисовки
+    public bool sticky = false;
+    abstract public void Resize();
+    abstract public void SetXY(int _x, int _y);
+    abstract public void OffsetXY(int _x, int _y);
+    abstract public void SetColor(Color c);
+    abstract public void Grow(int gr);
+    abstract public void DrawObj(System.Drawing.Graphics e);
+    abstract public void DrawRectangle(System.Drawing.Graphics e, Pen pen);
+    abstract public bool Find(int _x, int _y);
+    abstract public Rectangle GetRectangle();  //получить границы фигуры для контроля выхода за пределы
+}
+
+public class CCircle : Shape
+{
+    protected Color color;
+    protected int R;
+    public CCircle()
+    {
+
+        X = 0;
+        Y = 0;
+        R = 0;
+    }
+    public CCircle(int x, int y, int r, Color c, int Width, int Height)
+    {
+
+        this.X = x;
+        this.Y = y;
+        width = Width;
+        height = Height;
+        color = c;
+        if (r > x) r = x;
+        if (x + r > width) r = width - x;
+        if (r > y) r = y;
+        if (y + r > height) r = height - y;
+        R = r;
+        rect = new Rectangle(x - R, y - R, 2 * R, 2 * R);
+
+    }
+    public override void SetXY(int x, int y)
+    {
+        throw new NotImplementedException();
+    }
+    public override void Resize()
+    {
+        rect = new Rectangle(X - R, Y - R, 2 * R, 2 * R);
+    }
+
+    public override void Grow(int inc)
+    {
+        if (R + inc < X && X + R + inc < width && R + inc < Y && Y + R + inc < height && R + inc > 0) R += inc;
+        Resize();
+    }
+
+    public override void SetColor(Color c)
+    {
+        color = c;
+    }
+
+    public override void DrawObj(Graphics e)
+    {
+        e.FillEllipse(new SolidBrush(color), rect);
+    }
+
+    public override void DrawRectangle(Graphics e, Pen pen)
+    {
+        e.DrawRectangle(pen, rect);
+    }
+
+    public override void OffsetXY(int _x, int _y)
+    {
+        if (X + _x > R && X + _x + R < width) X += _x;
+        if (Y + _y > R && Y + _y + R < height) Y += _y;
+        Resize();
+    }
+    public override Rectangle GetRectangle()
+    {
+        return rect;
+    }
+
+    public override bool Find(int _x, int _y)
+    {
+        if (Math.Pow(X - _x, 2) + Math.Pow(Y - _y, 2) <= R * R) return true; else return false;
+    }
+}
+public class Square : CCircle
+{
+    private int n = 4;
+    List<PointF> first;
+
+    public Square(int x, int y, int r, Color c, int Width, int Height) : base(x, y, r, c, Width, Height)
+    {
+
+        if (r > x) r = x;
+        if (x + r > width) r = width - x;
+        if (r > y) r = y;
+        if (y + r > height) r = height - y;
+        Resize();
+    }
+
+    public override void DrawObj(Graphics e)
+    {
+        e.FillPolygon(new SolidBrush(color), first.ToArray());
+    }
+
+    public override void DrawRectangle(Graphics e, Pen pen)
+    {
+        e.DrawRectangle(pen, rect);
+    }
+
+    public override Rectangle GetRectangle()
+    {
+        return rect;
+    }
+
+    public override void Grow(int inc)
+    {
+        if (R + inc < X && X + R + inc < width && R + inc < Y && Y + R + inc < height && R + inc > 0) R += inc;
+        Resize();
+    }
+
+    public override void OffsetXY(int _x, int _y)
+    {
+        if (X + _x > R && X + _x + R < width) X += _x;
+        if (Y + _y > R && Y + _y + R < height) Y += _y;
+        Resize();
+    }
+
+    public override void Resize()
+    {
+        first = null;
+        first = new List<PointF>();
+        for (int i = 0; i < 360; i += 360 / n)
+        {
+            double radiani = (double)(i * 3.14) / 180;
+            float xx = X + (int)(R * Math.Cos(radiani));
+            float yy = Y + (int)(R * Math.Sin(radiani));
+            first.Add(new PointF(xx, yy));
+        }
+        rect = new Rectangle(X - R, Y - R, 2 * R, 2 * R);
+    }
+
+    public override void SetColor(Color c)
+    {
+        color = c;
+    }
+
+    public override void SetXY(int _x, int _y)
+    {
+
+    }
+
+    public override bool Find(int _x, int _y)
+    {
+        if (rect.X < _x && _x < rect.Right && rect.Y < _y && _y < rect.Bottom) return true; else return false;
+    }
+}
+
+public class Triangle : CCircle
+{
+    private int n = 3;
+    List<PointF> first;
+
+    public Triangle(int x, int y, int r, Color c, int Width, int Height) : base(x, y, r, c, Width, Height)
+    {
+
+        if (r > x) r = x;
+        if (x + r > width) r = width - x;
+        if (r > y) r = y;
+        if (y + r > height) r = height - y;
+        Resize();
+    }
+
+    public override void DrawObj(Graphics e)
+    {
+        e.FillPolygon(new SolidBrush(color), first.ToArray());
+    }
+
+    public override void DrawRectangle(Graphics e, Pen pen)
+    {
+        e.DrawRectangle(pen, rect);
+    }
+
+    public override Rectangle GetRectangle()
+    {
+        return rect;
+    }
+
+    public override void Grow(int inc)
+    {
+        if (R + inc < X && X + R + inc < width && R + inc < Y && Y + R + inc < height && R + inc > 0) R += inc;
+        Resize();
+    }
+
+    public override void OffsetXY(int _x, int _y)
+    {
+        if (X + _x > R && X + _x + R < width) X += _x;
+        if (Y + _y > R && Y + _y + R < height) Y += _y;
+        Resize();
+    }
+
+    public override void Resize()
+    {
+        first = null;
+        first = new List<PointF>();
+        for (int i = 0; i < 360; i += 360 / n)
+        {
+            double radiani = (double)(i * 3.14) / 180;
+            float xx = X + (int)(R * Math.Cos(radiani));
+            float yy = Y + (int)(R * Math.Sin(radiani));
+            first.Add(new PointF(xx, yy));
+        }
+        rect = new Rectangle(X - R, Y - R, 2 * R, 2 * R);
+    }
+
+    public override void SetColor(Color c)
+    {
+        color = c;
+    }
+
+    public override void SetXY(int _x, int _y)
+    {
+
+    }
+
+    public override bool Find(int _x, int _y)
+    {
+        if (rect.X < _x && _x < rect.Right && rect.Y < _y && _y < rect.Bottom) return true; else return false;
+    }
+}
+
+
 
 public class ObjObserved
 {
