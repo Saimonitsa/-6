ed01d55e9cfc64eb87e3fa71c7f633f2ed3bb0cf
Saimonitsa <95011357+Saimonitsa@users.noreply.github.com>
2022-01-19 21:47:46 +0500

    2.3

--- лаба ооп6/Form1.cs
+++ лаба ооп6/Form1.cs
@@ -167,6 +167,226 @@ public abstract class Shape : ObjObserved
     abstract public Rectangle GetRectangle();  //получить границы фигуры для контроля выхода за пределы
 }
 
+public class SGroup : Shape
+{
+    public Storage<Shape> sto;
+
+    public SGroup()
+    {
+        sto = new Storage<Shape>();
+        name = "Group";
+    }
+
+    public SGroup(int Width, int Height)
+    {
+        width = Width;
+        height = Height;
+        sto = new Storage<Shape>();
+        name = "Group";
+    }
+
+    public void Add(Shape s)
+    {
+        sto.add(s);
+        sto.get().sticky = false;
+        if (sto.size() == 1) rect = new Rectangle(s.GetRectangle().X, s.GetRectangle().Y, s.GetRectangle().Width, s.GetRectangle().Height);
+        else
+        {
+            if (s.GetRectangle().Left < rect.Left)
+            {
+                int tmp = rect.Right;
+                rect.X = s.GetRectangle().Left;
+                rect.Width = tmp - rect.X;
+            }
+            if (s.GetRectangle().Right > rect.Right) rect.Width = s.GetRectangle().Right - rect.X;
+            if (s.GetRectangle().Top < rect.Top)
+            {
+                int tmp = rect.Bottom;
+                rect.Y = s.GetRectangle().Top;
+                rect.Height = tmp - rect.Y;
+            }
+            if (s.GetRectangle().Bottom > rect.Bottom) rect.Height = s.GetRectangle().Bottom - rect.Y;
+        }
+    }
+
+    public Shape Out()
+    {
+        if (sto.size() != 0)
+        {
+            Shape tmp = sto.get();
+            sto.del();
+            Resize();
+            return tmp;
+        }
+        return null;
+    }
+
+    public int size()
+    {
+        return sto.size();
+    }
+
+    public override void Resize()
+    {
+        if (sto.size() != 0)
+        {
+            sto.toFirst();
+            rect = sto.getIterator().GetRectangle();
+            for (int i = 0; i < sto.size(); i++, sto.next())
+            {
+                if (sto.getIterator().GetRectangle().Left < rect.Left)
+                {
+                    int tmp = rect.Right;
+                    rect.X = sto.getIterator().GetRectangle().Left;
+                    rect.Width = tmp - rect.X;
+                }
+                if (sto.getIterator().GetRectangle().Right > rect.Right) rect.Width = sto.getIterator().GetRectangle().Right - rect.X;
+                if (sto.getIterator().GetRectangle().Top < rect.Top)
+                {
+                    int tmp = rect.Bottom;
+                    rect.Y = sto.getIterator().GetRectangle().Top;
+                    rect.Height = tmp - rect.Y;
+                }
+                if (sto.getIterator().GetRectangle().Bottom > rect.Bottom) rect.Height = sto.getIterator().GetRectangle().Bottom - rect.Y;
+            }
+        }
+    }
+
+    public override void Clone()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void DrawObj(Graphics e)
+    {
+        if (sto.size() != 0)
+        {
+            sto.toFirst();
+            for (int i = 0; i < sto.size(); i++, sto.next()) sto.getIterator().DrawObj(e);
+        }
+    }
+
+    public override void Grow(int gr)
+    {
+        if (sto.size() != 0)
+        {
+            if (gr > 0 && rect.X + gr > 1 && gr + rect.Right < width - 1 && rect.Y + gr > 1 && gr + rect.Bottom < height - 1)
+            {
+                rect.X -= gr;
+                rect.Y -= gr;
+                rect.Width += 2 * gr;
+                rect.Height += 2 * gr;
+                sto.toFirst();
+                for (int i = 0; i < sto.size(); i++, sto.next()) sto.getIterator().Grow(gr);
+            }
+            if (gr < 0)
+            {
+                sto.toFirst();
+                for (int i = 0; i < sto.size(); i++, sto.next()) sto.getIterator().Grow(gr);
+                if (gr < 0) Resize();
+            }
+        }
+    }
+
+    public override void OffsetXY(int _x, int _y)
+    {
+        if (sto.size() != 0)
+        {
+            if (rect.X + _x > 0 && _x + rect.Right < width)
+            {
+                rect.X += _x;
+                sto.toFirst();
+                for (int i = 0; i < sto.size(); i++, sto.next()) sto.getIterator().OffsetXY(_x, 0);
+            }
+            if (rect.Y + _y > 0 && _y + rect.Bottom < height)
+            {
+                rect.Y += _y;
+                sto.toFirst();
+                for (int i = 0; i < sto.size(); i++, sto.next()) sto.getIterator().OffsetXY(0, _y);
+            }
+        }
+    }
+
+    public override void SetColor(Color c)
+    {
+        if (sto.size() != 0)
+        {
+            sto.toFirst();
+            for (int i = 0; i < sto.size(); i++, sto.next()) sto.getIterator().SetColor(c);
+        }
+    }
+
+    public override void SetXY(int _x, int _y)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Rectangle GetRectangle()
+    {
+        return rect;
+    }
+
+    public override bool Find(int _x, int _y)
+    {
+        if (rect.X < _x && _x < rect.Right && rect.Y < _y && _y < rect.Bottom) return true; else return false;
+    }
+
+    public override void DrawRectangle(Graphics e, Pen pen)
+    {
+        e.DrawRectangle(pen, rect);
+    }
+
+    public override void Save(StreamWriter stream)
+    {
+        stream.WriteLine("Group");
+        stream.WriteLine(sto.size() + " " + width + " " + height);
+        if (sto.size() != 0)
+        {
+            sto.toFirst();
+            for (int i = 0; i < sto.size(); i++, sto.next()) sto.getIterator().Save(stream);
+        }
+    }
+
+    public override void Load(StreamReader stream)
+    {
+        ShapeFactory factory = new ShapeFactory();
+        string[] data = stream.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        int i = Convert.ToInt32(data[0]);
+        width = Convert.ToInt32(data[1]);
+        height = Convert.ToInt32(data[2]);
+        for (; i > 0; i--)
+        {
+            Shape tmp = factory.createShape(stream.ReadLine());
+            tmp.Load(stream);
+            Add(tmp);
+        }
+    }
+
+    public override string GetInfo()
+    {
+        return name + "    Size : " + sto.size().ToString();
+    }
+
+    public override void Rotate(int gr)
+    {
+        if (sto.size() != 0)
+        {
+            sto.toFirst();
+            for (int i = 0; i < sto.size(); i++, sto.next()) sto.getIterator().Rotate(gr);
+        }
+    }
+
+    public override bool Find(Shape obj)
+    {
+        if (sto.size() != 0)
+        {
+            sto.toFirst();
+            for (int i = 0; i < sto.size(); i++, sto.next()) if (sto.getIterator().Find(obj) == true) return true;
+        }
+        return false;
+    }
+}
+
 public class CCircle : Shape
 {
     protected Color color;
@@ -620,6 +840,11 @@ class Tree : Watch
             int SelectedIndex = 0;
             TreeNode start = new TreeNode("Фигуры");
             sto.toFirst();
+            for (int i = 0; i < sto.size(); i++, sto.next())
+            {
+                if (sto.getCurPTR() == sto.getIteratorPTR()) SelectedIndex = i;
+                PrintNode(start, sto.getIterator());
+            }
             tree.Nodes.Add(start);
 
             for (int i = 0; i < sto.size(); i++)
@@ -633,6 +858,25 @@ class Tree : Watch
         }
         tree.ExpandAll();
 
+    }
+    private void PrintNode(TreeNode node, Shape shape)
+    {
+        if (shape is SGroup)
+        {
+            TreeNode tn = new TreeNode(shape.GetInfo());
+            if (((SGroup)shape).sto.size() != 0)
+            {
+                ((SGroup)shape).sto.toFirst();
+                for (int i = 0; i < ((SGroup)shape).sto.size(); i++, ((SGroup)shape).sto.next())
+                    PrintNode(tn, ((SGroup)shape).sto.getIterator());
+            }
+            node.Nodes.Add(tn);
+        }
+        else
+        {
+
+            node.Nodes.Add(shape.GetInfo());
+        }
     }
 
 
